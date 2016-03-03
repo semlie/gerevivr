a426306a4f03a8a34596740f212c95d6decf8736
semlie <israellieb@gmail.com>
2016-03-04 01:23:35 +0200

    added the stract of the flow

---
+++ services/call_flow.php
@@ -0,0 +1,47 @@
+<?php
+require '../models/product.php';
+/* 
+ *
+ * 
+ */
+
+function init_call_flow(){
+    
+}
+
+function is_call_identified(){
+    
+}
+
+function is_user_entered_digits($results){
+    
+}
+
+function throw_error_messege($err_file_name,$next_step){
+    
+}
+
+function get_product_by_id($product_id){
+    
+}
+
+function is_product_id_valid($product_id){
+    
+}
+
+function read_product_details(product $product,$next){
+    
+}
+
+function validit_quntity($qty,$next){
+    
+}
+
+function read_total_order(){
+    
+}
+
+function read_and_ask($state,$nextOK,$nextErr){
+    
+}
+
