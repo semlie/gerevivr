a596cfd0fd1346c6bb6e42ea208f5db14c5f41e4
semlie <israellieb@gmail.com>
2016-03-20 01:16:35 +0200

    add call flow manager

--- models/product.php
+++ models/product.php
@@ -1,16 +1,6 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of product
- *
- * @author Admin
- */
+require './ModelInfo.php';
 class product extends ModelInfo{
     //put your code here
    public  $Name,$description,$CatalogNumber,$FirstCategory, $SecondaryCategory,  $Material, $Thickness ,$Color ,$Size, $Price ,$Manufacturer, $Brand ,$Strength, $Example;
