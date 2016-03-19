a8b6d593bcd04bac0b2af011dc6c1658b83463e6
semlie <israellieb@gmail.com>
2016-03-20 01:13:49 +0200

    add call flow manager

--- services/call_flow.php
+++ services/call_flow.php
@@ -7,18 +7,29 @@ require '../models/product.php';
  */
 
 class callFlow_manager {
-    
+
+    const MAX_DIGIT = 8;
+    CONST TIME_OUT = 15000;
+    CONST MAX_CYCLES = 4;
+
     public $agi;
-    function __construct() {
-        
+
+    function __construct($agi) {
+        $this->agi = $agi;
     }
 
     public function init_call_flow() {
-        
+        $this->agi->answer();
+        $cid = $this->agi->parse_callerid();
+        $this->is_call_identified($cid);
     }
 
-    public function is_call_identified() {
-        
+    public function is_call_identified($cid) {
+        if (!empty($cid)) {
+            $this->agi->text2wav('The value must be a constant expression, not for example a variable, a property, or a function call.');
+        } else {
+            return FALSE;
+        }
     }
 
     public function is_user_entered_digits($results) {
@@ -51,6 +62,26 @@ class callFlow_manager {
 
     public function read_and_ask($state, $nextOK, $nextErr) {
         
+    }
+
+    private function getData($playFile, $onErr, $maxDigit = self::MAX_DIGIT) {
+        $cycle = 0;
+
+        do {
+            $cycle ++;
+            $result = $this->agi->get_data($playFile, self::TIME_OUT, $maxDigit);
+            $this->agi->conlog("Failed to ping {$result['result']}");
+        } while (!empty($result['result']) && $cycle < self::MAX_CYCLES);
+
+        if (self::MAX_CYCLES - 1 == $cycle) {
+            if (function_exists($onErr)) {
+                $onErr();
+            } else {
+            return FALSE;    
+            }
+        } else {
+            return $result['result'];
+        }
     }
 
 }
